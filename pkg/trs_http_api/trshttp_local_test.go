f8716fb1b7d3833cc58fb851f0b15a429525f511
Joshua Williams <joshuaw@hpe.com>
2024-11-10 13:44:39 -0800

    Disable http2

--- pkg/trs_http_api/trshttp_local_test.go
+++ pkg/trs_http_api/trshttp_local_test.go
@@ -37,6 +37,8 @@ import (
 	"testing"
 	"time"
 
+	"golang.org/x/net/http2"
+
 	base "github.com/Cray-HPE/hms-base/v2"
 	"github.com/sirupsen/logrus"
 )
@@ -574,6 +576,7 @@ func testPCSUseCase(t *testing.T, httpTimeout time.Duration, cPolicy ClientPolic
         ReadTimeout: 0,
         WriteTimeout: 0,
     }
+	http2.ConfigureServer(successSrv, &http2.Server{MaxConcurrentStreams: 0})
 	go successSrv.ListenAndServe()
 
 	// Create an http request for tasks that complete successfully
