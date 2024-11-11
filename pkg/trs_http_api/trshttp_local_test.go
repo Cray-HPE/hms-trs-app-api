69d0af33573374e6e5ddd1066708f4eb88bbbbf8
Joshua Williams <joshuaw@hpe.com>
2024-11-11 12:39:15 -0800

    arst

--- pkg/trs_http_api/trshttp_local_test.go
+++ pkg/trs_http_api/trshttp_local_test.go
@@ -698,15 +698,13 @@ tList := successList
 		<-taskListChannel
 	}
 
-/*
 for _, tsk := range(tList) {
 	if tsk.Request.Response != nil && tsk.Request.Response.Body != nil {
 		t.Logf("Closing response body for task %v", tsk.Request.URL)
 		tsk.Request.Response.Body.Close()
-		tsk.Request.Response.Body = nil
+//		tsk.Request.Response.Body = nil
 	}
 }
-*/
 //tloc.Cancel(&tList)
 	// The only remaining connections should be for the stalled tasks
 	// which should still be in ESTABLISHED
