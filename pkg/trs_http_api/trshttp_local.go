06e9345abdc41bb8ed4ccbca80b866d1b2a6192c
Joshua Williams <joshuaw@hpe.com>
2024-11-15 12:17:11 -0800

    test

--- pkg/trs_http_api/trshttp_local.go
+++ pkg/trs_http_api/trshttp_local.go
@@ -56,10 +56,8 @@ const (
 func (tloc *TRSHTTPLocal) Init(serviceName string, logger *logrus.Logger) error {
 	if logger != nil {
 		tloc.Logger = logger
-		tloc.Logger.Errorf("JW_DEBUG: Reusing passed in logger")
 	} else {
 		tloc.Logger = logrus.New()
-		tloc.Logger.Errorf("JW_DEBUG: using new logger")
 	}
 
 	tloc.ctx, tloc.ctxCancelFunc = context.WithCancel(context.Background())
