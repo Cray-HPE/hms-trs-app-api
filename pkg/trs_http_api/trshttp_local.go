2e0f3303fb32c131f1040cd19db4fdcda50b0e62
Joshua Williams <joshuaw@hpe.com>
2024-11-17 20:13:51 -0800

    Test

--- pkg/trs_http_api/trshttp_local.go
+++ pkg/trs_http_api/trshttp_local.go
@@ -227,7 +227,7 @@ type trsRoundTripper struct {
 func (c *trsRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	resp, err := c.transport.RoundTrip(req)
 
-	TESTLOGGER.Warnf("-----------------> RoundTrip: err=%v", err)
+	TESTLOGGER.Warnf("-----------------> RoundTrip: err=%v type=%T", err, err)
 
 	if err != nil {
 		//c.skipCICsMutex.Lock()
